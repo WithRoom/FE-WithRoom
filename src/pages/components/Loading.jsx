7ad4eaffbc4180f54a7a06733f12429ec4296dc7
MinWook6457 <minuk6457@gmail.com>
2024-11-02 01:30:48 +0900

    [경로수정] : gif 경로 src 하위 -> public

--- src/pages/components/Loading.jsx
+++ src/pages/components/Loading.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Background, LoadingText } from './Style.js';
-import Spinner from '../../test/spinner.gif';
-
 const Loading = () => {
   return (
     <Background>
       <LoadingText>잠시만 기다려 주세요.</LoadingText>
-      <img src={Spinner} alt="로딩중" width="5%" />
+      <img src='/spinner.gif' alt="로딩중" width="5%" />
     </Background>
   );
 };
