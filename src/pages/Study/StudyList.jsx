7293e7cf31c45c35fa83a09a78386c23669f90ab
MinWook6457 <minuk6457@gmail.com>
2024-11-04 22:48:55 +0900

    [오류수정] : 스터디 리스트 정보 무한 요청 수정

--- src/pages/Study/StudyList.jsx
+++ src/pages/Study/StudyList.jsx
@@ -24,7 +24,7 @@ const StudyList = () => {
     if (initialStudies.length === 0) {
       fetchAllStudies();
     }
-  }, [initialStudies]);
+  }, []);
 
   const fetchAllStudies = async () => {
     try {
